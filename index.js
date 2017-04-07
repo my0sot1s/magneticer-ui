93351d42c1ebe98678ef80a3efc03b5e8260a886
ubuntu-Pc <manhte231@gmail.com>
2017-04-07 08:37:46 +0700

    fix 1

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var path = require('path');
+var http = require('http');
 var app = express()
 app.use(express.static(__dirname + '/build'));
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '/build', 'index.html'))
 })
+const port = process.env.PORT || 3000;
 app.use(express.static('public'))
-app.listen(3000, function () {
-  console.log('Dev run app listening on port 3000!')
+var server = http.createServer(app);
+server.listen(port, function () {
+  console.log('Dev run app listening on port:' + port);
 })
